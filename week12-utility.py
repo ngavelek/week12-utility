585ecaa33224432851277ce82e1d5401d6e08ab0
nategavelek <ngavelek@mymail.mines.edu>
2019-11-21 21:07:28 -0700

    This is is my seventh function

--- week12-utility.py
+++ week12-utility.py
@@ -41,10 +41,10 @@ def Intersection(list1, list2):
     overLapList = []
     for i in range(len(list1)):
         for j in range(len(list2)):
-            print (list1[i], list2[j])
             if list1[i] == list2[j]:
                overLapList.append(list1[i])
-
     return overLapList
+
+
 
 
